3735ef04d2502e998b2fade87946a80f4949f9e7
WojciechNowakowski <wojtek.nowakowski15@wp.pl>
2014-10-18 14:50:41 +0200

    Wojtek change

--- demoLeg.cpp
+++ demoLeg.cpp
@@ -1,3 +1,8 @@
+/*	Autorzy:
+		£ukasz Mej³un
+		Wojciech Nowakowski
+*/
+
 #include "include/defs/defs.h"
 #include "include/legModel/leg.h"
 #include <iostream>
@@ -7,13 +12,17 @@ using namespace std;
 
 int main( int argc, const char** argv )
 {
-    try {
-         //Grabber* grabber;
-         //grabber = createFileGrabber();
-    }
-    catch (const std::exception& ex) {
-        std::cerr << ex.what() << std::endl;
-        return 1;
-    }
-    return 0;
+	try 
+	{
+		//Grabber* grabber;
+		//grabber = createFileGrabber();
+		cout << "DEMO STEROWNIKA NOGI";
+		getchar();
+	}
+  catch (const std::exception& ex) 
+	{
+		std::cerr << ex.what() << std::endl;
+    return 1;
+  }
+  return 0;
 }
