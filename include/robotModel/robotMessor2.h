f9034db1bdf136678eb3838e6ecbddabab987b0b
maciejokoniewski <okoniewski.maciej@gmail.com>
2014-10-30 16:38:27 +0100

    Jerzy_Wiatrow_fix

--- include/robotModel/robotMessor2.h
+++ include/robotModel/robotMessor2.h
@@ -2,15 +2,48 @@
 //Emil Waledziak
 
 
+namespace controller {
+    /// Kinematic interface
+    class Robot {
+        public:
 
+            /// Robot type
+            enum Type {
+                /// Messor Robot
+                TYPE_MESSOR,
+                /// Messor2 Robot
+                TYPE_MESSOR2,
+                /// StarlETH robot
+                TYPE_STARLETH,
+            };
 
+            /// overloaded constructor
+            Robot(const std::string _name, Type _type) : name(_name), type(_type) {};
 
+            Robot(std::string configFilename, const std::string _name, Type _type) : name(_name), type(_type){
 
-///Compute configuration of the robot for the reference motion
-virtual std::vector<float_type> movePlatform(const Mat34& motion) = 0;
+            }
 
-///Compute configuration of the robot for the reference motion (in relation to neutral pose)
-virtual std::vector<float_type> movePlatformNeutral(const Mat34 motion) = 0;
+            /// Name of the robot model
+            virtual const std::string& getName() const { return name; }
 
-///Compute force in each joint of the legs, input configuration of the robot
-virtual std::vector<float_type> computeCompliance(std::vector<float_type>) = 0;
+            ///Compute configuration of the robot for the reference motion
+            virtual std::vector<float_type> movePlatform(const Mat34& motion) = 0;
+
+            ///Compute configuration of the robot for the reference motion (in relation to neutral pose)
+            virtual std::vector<float_type> movePlatformNeutral(const Mat34 motion) = 0;
+
+            ///Compute force in each joint of the legs, input configuration of the robot
+            virtual std::vector<float_type> computeCompliance(std::vector<float_type>) = 0;
+
+            /// Virtual descrutor
+            virtual ~Robot() {}
+
+        protected:
+            /// Robot type
+            Type type;
+
+            /// Robot name
+            const std::string name;
+    };
+};
