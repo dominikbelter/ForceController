1def8442451f1fee26da8ef9064cfbcd5a3b0d8b
maciejokoniewski <okoniewski.maciej@gmail.com>
2014-11-06 15:44:49 +0100

    done by MO EW JW MZ

--- src/robotModel/robotMessor2.cpp
+++ src/robotModel/robotMessor2.cpp
@@ -8,13 +8,21 @@ using namespace controller;
 
 
 ///Compute configuration of the robot for the reference motion
- std::vector<float_type> movePlatform(const Mat34& motion)
+/**
+* @param motion
+* @return tmp
+*/
+std::vector<float_type> movePlatform(const Mat34& motion)
 {
     std::vector<float_type> tmp;
      return tmp;
 }
 		
 ///Compute configuration of the robot for the reference motion (in relation to neutral pose)
+/**
+* @param motion
+* @return tmp
+*/
  std::vector<float_type> movePlatformNeutral(const Mat34 motion)
 {
      std::vector<float_type> tmp;
@@ -22,6 +30,10 @@ using namespace controller;
 }
 
 ///Compute force in each joint of the legs, input configuration of the robot
+ /**
+ * @param vector<float_type>
+ * @return tmp
+ */
  std::vector<float_type> computeCompliance(std::vector<float_type>)
 {
    
