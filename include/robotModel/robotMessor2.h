ad2d2da93800b69989317c648f0d917ee2df767f
maciejokoniewski <okoniewski.maciej@gmail.com>
2014-11-06 15:49:28 +0100

    done by MO EW JW MZ

--- include/robotModel/robotMessor2.h
+++ include/robotModel/robotMessor2.h
@@ -28,12 +28,24 @@ public:
     const std::string& getName() const { return name; }
 
     ///Compute configuration of the robot for the reference motion
+    /**
+    * @param motion
+    * @return tmp
+    */
     std::vector<float_type> movePlatform(const Mat34& motion);
 
     ///Compute configuration of the robot for the reference motion (in relation to neutral pose)
+    /**
+    * @param motion
+    * @return tmp
+    */
     std::vector<float_type> movePlatformNeutral(const Mat34 motion);
 
     ///Compute force in each joint of the legs, input configuration of the robot
+    /**
+    * @param vector<float_type>
+    * @return tmp
+    */
     std::vector<float_type> computeCompliance(std::vector<float_type>);
 private:
 
