4d38a29a3a4b17213f94da8c4ec057717457c075
tt <tomlocks@gmail.com>
2014-11-07 11:48:04 -0800

    Added Irrlicht library

--- demoVisualization.cpp
+++ demoVisualization.cpp
@@ -2,11 +2,38 @@
 #include "include/visualization/visualizer.h"
 #include <iostream>
 #include <stdio.h>
+#include <irrlicht.h>
+
+
+// Irrlicht installation
+//
+// sudo apt-get install libIrrlicht1.8 libIrrlicht-dev
 
 using namespace std;
 
+
+using namespace irr;
+
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+
+
 int main( int argc, const char** argv )
 {
+
+    IrrlichtDevice *device =
+        createDevice( video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16,
+            false, false, false, 0);
+
+    if (!device)
+        return 1;
+
+    getchar();
+
     try {
          //Grabber* grabber;
          //grabber = createFileGrabber();
