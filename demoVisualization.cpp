635bc414ac63e6e20e11f1023c9519816134f132
AveAmore <sebastian.drogowski@gmail.com>
2014-11-12 12:31:31 +0100

    Added robot parts (visualisation)

--- demoVisualization.cpp
+++ demoVisualization.cpp
@@ -22,25 +22,67 @@ using namespace gui;
 
 
 
-int main( int argc, const char** argv )
+int main(int argc, const char** argv)
 {
 
-    IrrlichtDevice *device =
-        createDevice( video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16,
-            false, false, false, 0);
+	IrrlichtDevice *device =
+		createDevice(video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16,
+		false, false, false, 0);
+	if (!device)
+	return 1;
 
-    if (!device)
-        return 1;
+	video::IVideoDriver * video = device->getVideoDriver();
+	scene::ISceneManager * menage = device->getSceneManager();
+	scene::ICameraSceneNode * kam = menage->addCameraSceneNodeFPS();
+	device->getCursorControl()->setVisible(true);
+	kam->setPosition(core::vector3df(0, 0, -100));
+	//Zasiêg pola widzenia kamery
+	//kam->setFarValue( 90000 );
+	//Wczytywanie modelu
+	IAnimatedMesh * coxa = menage->getMesh("/home/tomlock/Desktop/ForceController/src/visualization/coxa.stl");
+	IAnimatedMeshSceneNode * cx = menage->addAnimatedMeshSceneNode(coxa);
+	IAnimatedMeshSceneNode * cx2 = menage->addAnimatedMeshSceneNode(coxa);
+	cx->setScale(core::vector3df(-30, -30, -30));
+	cx->setPosition(core::vector3df(0, 0, 0));
+	cx->setMaterialFlag(video::EMF_LIGHTING, false);
+	cx->setMaterialTexture(0, video->getTexture("media/img/pudlo.png"));
 
-    getchar();
+	cx2->setScale(core::vector3df(-30, -30, -30));
+	cx2->setPosition(core::vector3df(0, 300, 0));
+	cx2->setMaterialFlag(video::EMF_LIGHTING, false);
+	cx2->setMaterialTexture(0, video->getTexture("media/img/pudlo.png"));
 
-    try {
-         //Grabber* grabber;
-         //grabber = createFileGrabber();
-    }
-    catch (const std::exception& ex) {
-        std::cerr << ex.what() << std::endl;
-        return 1;
-    }
-    return 0;
+	IAnimatedMesh * vitulus = menage->getMesh("/home/tomlock/Desktop/ForceController/src/visualization/vitulus.stl");
+	IAnimatedMeshSceneNode * vit = menage->addAnimatedMeshSceneNode(vitulus);
+	vit->setScale(core::vector3df(-30, -30, -30));
+	vit->setRotation(core::vector3df(90, 90, 0));
+	vit->setPosition(core::vector3df(200, 0, 0));
+	vit->setMaterialFlag(video::EMF_LIGHTING, false);
+	vit->setMaterialTexture(0, video->getTexture("media/img/pudlo.png"));
+
+	IAnimatedMesh * femur = menage->getMesh("/home/tomlock/Desktop/ForceController/src/visualization/femur.stl");
+	IAnimatedMeshSceneNode * fe = menage->addAnimatedMeshSceneNode(femur);
+	fe->setScale(core::vector3df(-30, -30, -30));
+	fe->setRotation(core::vector3df(90, 90, 0));
+	fe->setPosition(core::vector3df(200, 300, 0));
+	fe->setMaterialFlag(video::EMF_LIGHTING, false);
+	fe->setMaterialTexture(0, video->getTexture("media/img/pudlo.png"));
+	// wczytywanie korpusu
+	IAnimatedMesh * korpus = menage->getMesh("/home/tomlock/Desktop/ForceController/src/visualization/korpus2.stl");
+	IAnimatedMeshSceneNode * kor = menage->addAnimatedMeshSceneNode(korpus);
+	kor->setScale(core::vector3df(-30, -30, -30));
+	kor->setRotation(core::vector3df(90, 90, 0));
+	kor->setPosition(core::vector3df(200, 600, 0));
+	kor->setMaterialFlag(video::EMF_LIGHTING, false);
+	kor->setMaterialTexture(0, video->getTexture("media/img/pudlo.png"));
+
+	while (device->run())
+
+	{
+		video->beginScene(true, true, video::SColor(255, 0, 10, 200));
+		menage->drawAll();
+		video->endScene();
+	}
+	//device->drop();
+	return 0;
 }
