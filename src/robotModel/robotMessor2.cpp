b5edf8c1ffb2b664800d87b246e5c9ff03ca766c
maciejokoniewski <okoniewski.maciej@gmail.com>
2014-11-13 17:48:32 +0100

    fixed

--- src/robotModel/robotMessor2.cpp
+++ src/robotModel/robotMessor2.cpp
@@ -20,14 +20,14 @@ RobotMessor::~RobotMessor(void)
 }
 
 ///Compute configuration of the robot for the reference motion
-std::vector<float_type> movePlatform(const Mat34& motion)
+std::vector<float_type> RobotMessor::movePlatform(const Mat34& motion)
 {
     std::vector<float_type> tmp;
      return tmp;
 }
 		
 ///Compute configuration of the robot for the reference motion (in relation to neutral pose)
- std::vector<float_type> movePlatformNeutral(const Mat34 motion)
+ std::vector<float_type> RobotMessor::movePlatformNeutral(const Mat34 motion)
 {
      std::vector<float_type> tmp;
       return tmp;
@@ -40,10 +40,10 @@ std::vector<Mat34> RobotMessor::conputeLinksPosition(std::vector<float_type> con
  }
 
 ///Compute force in each joint of the legs, input configuration of the robot
- std::vector<float_type> computeCompliance(std::vector<float_type>)
+ std::vector<float_type> RobotMessor::computeCompliance(std::vector<float_type>)
 {
    
-     std::vector<float_type> tmp;
+    std::vector<float_type> tmp;
       return tmp;
 
 }
