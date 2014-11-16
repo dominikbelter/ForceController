6668719ce0667904484479d5b30af3077ea1ea11
NorbertWerblinski <verb@op.pl>
2014-11-16 09:24:45 +0100

    XML kinematics config loader

--- demoKinematic.cpp
+++ demoKinematic.cpp
@@ -1,15 +1,21 @@
 #include "include/defs/defs.h"
 #include "include/kinematic/kinematic.h"
+#include "../include/kinematic/kinematicLie.h"
 #include <iostream>
 #include <stdio.h>
 
 using namespace std;
+using namespace tinyxml2;//--------------------
 
 int main( int argc, const char** argv )
 {
     try {
          //Grabber* grabber;
          //grabber = createFileGrabber();
+		controller::Kinematic* demoKine;
+		demoKine = createKinematicLie("legModel.xml");
+		std::cout << "Kinematic type: " << demoKine->getName() << "\n";
+		
     }
     catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
