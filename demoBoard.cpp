98dd8ac359851e30dd3f6b8017eadd6e8fe4ac82
Limek <limek91@gmail.com>
2014-12-04 16:03:07 +0100

    setPos

--- demoBoard.cpp
+++ demoBoard.cpp
@@ -17,20 +17,18 @@ using namespace controller;
 
 int main( int argc, const char** argv )
 {
-    try {
-        Board* board = createBoardDynamixel();
-         //Grabber* grabber;
-         //grabber = createFileGrabber();
-    }
-    catch (const std::exception& ex) {
-        std::cerr << ex.what() << std::endl;
-        return 1;
-    }
+    int deg;
 
-    //DB powinno byc: Board *board = new BoardDynamixel();
-    BoardDynamixel *demo = new BoardDynamixel();
-    //DB 10 jest nieprawidlowym parametrem, legNo powinien byc z przedzialu 0-5, a servoNo z przedzialu 0-2.
-    demo->setPosition(1, 10, M_PI/4);   //move 1st leg, 10 joint, about 45deg
+    Board *demo = new BoardDynamixel();
+    while (true){
+
+        cout<<"Enter degree"<<endl;
+        cin>>deg;
+        if (deg>100){
+            break;
+        }
+        demo->setPosition(1, 0, (M_PI*deg)/180);   //move 1st leg, 0 joint, about 45deg
+    }
     delete demo;
 
     return 0;
