4365faebc98ebd9a05ba969119ff19b9e7110455
tomlocks <tomlocks@gmail.com>
2014-12-15 08:06:49 -0800

    Zero orientation fix

--- src/visualization/visualizerIrrlicht.cpp
+++ src/visualization/visualizerIrrlicht.cpp
@@ -252,11 +252,11 @@ void VisualizerIrrlicht::drawLeg(int legIndex,  irr::core::vector3d<irr::f32> po
 
     video->drawMeshBuffer(coxaMeshBuffer);
 
-    ourTransform(0, -PI / 2 - configuration.at(3*(legIndex - 1) + 1), 0, 0, 0, 5);
+    ourTransform(0, -PI / 2 - configuration.at(3*(legIndex - 1) + 1) , 0, 0, 0, 5);
 
     video->drawMeshBuffer(femurMeshBuffer);
 
-    ourTransform(0, -PI / 2 - configuration.at(3*(legIndex - 1) + 2), 0, 11.2, 0, 5);
+    ourTransform(0, -PI / 2 - configuration.at(3*(legIndex - 1) + 2) - 1.1989, 0, 11.2, 0, 5);
 
     video->drawMeshBuffer(vitulusMeshBuffer);
 
