b72898cb40ecb4a41df7803e20e61702ee1863a5
Krzysztof Pelczynski <krzysztofpelczynski1991@gmail.com>
2014-12-18 15:42:31 +0100

    poprawki zajecia2

--- demoBoard.cpp
+++ demoBoard.cpp
@@ -32,30 +32,36 @@ int main( int argc, const char** argv )
     for(int i = 0; i < 18; i++ ){
         motorComplianceSlope.push_back(120);
     }
+    vector <float_type> polozeniePoczatkowe;
+    polozeniePoczatkowe.push_back( 0 );
+    polozeniePoczatkowe.push_back( 24 );
+    polozeniePoczatkowe.push_back( -114 );
+
 
     Board *demo = createBoardDynamixel();
+
     demo->readPosition( LEG_1, JOINT_0, kat0);
     demo->readPosition( LEG_1, JOINT_1, kat1);
     demo->readPosition( LEG_1, JOINT_2, kat2);
+
+    cout << kat0 << endl;
+
     demo->setSpeed( motorSpeed );
+
     demo->setComplianceSlope( motorComplianceSlope );
     demo->setOffset( motorOffset );
 
+    demo->setPosition(LEG_0, polozeniePoczatkowe );
+    //demo->setPosition(LEG_1, polozeniePoczatkowe );
+    //demo->setPosition(LEG_2, polozeniePoczatkowe );
+    //demo->setPosition(LEG_3, polozeniePoczatkowe );
+    //demo->setPosition(LEG_4, polozeniePoczatkowe );
+    //demo->setPosition(LEG_5, polozeniePoczatkowe );
+
+    cout << kat1 << endl << kat2 << endl;
 
     while (true){
 
-     /*   cout<<"Enter degree"<<endl;
-        cin>>deg;
-        if (deg>100){
-            break;
-        }
-        demo->setPosition(1, 0, (M_PI*deg)/180);   //move 1st leg, 0 joint, about 45deg
-        //demo->setPosition(4, 0, (M_PI*deg)/180);   //move 1st leg, 0 joint, about 45deg
-
-
-        cout << "Kat: "<< (kat*180)/M_PI << endl
-              << "Moment: " << moment << endl;
-       */
     }
 
     return 0;
