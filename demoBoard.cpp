41f5dceb233e6e97a37912c745032bb2e882c181
Krzysztof Pelczynski <krzysztofpelczynski1991@gmail.com>
2015-01-15 15:53:56 +0100

    proba

--- demoBoard.cpp
+++ demoBoard.cpp
@@ -5,30 +5,22 @@
 #include "3rdParty/dynamixel/dxl_hal.h"
 #include <iostream>
 #include <stdio.h>
-
 using namespace std;
 using namespace controller;
-
 int main( int argc, const char** argv )
 {
     float_type kat;
     float_type noga;
     float_type wezel;
-<<<<<<< HEAD
+    float_type podatnosc;
     float_type kat0 = 0;
-=======
-	float_type podatnosc;    
-float_type kat0 = 0;
->>>>>>> 7a307717e72710a801769c7f78674263d0f3b432
     float_type kat1 = 0;
     float_type kat2 = 0;
-float_type readkat = 0;
+    float_type readkat = 0;
     vector<float_type> readkatLeg;
-
     float_type moment0 = 0;
     float_type moment1 = 0;
     float_type moment2 = 0;
-
     vector <float_type> motorSpeed;
     for(int i = 0; i < 18; i++ ){
         motorSpeed.push_back(50);
@@ -45,70 +37,44 @@ float_type readkat = 0;
     polozeniePoczatkowe.push_back( 0 );
     polozeniePoczatkowe.push_back( 0 );
     polozeniePoczatkowe.push_back( 0 );
-
     Board *demo = createBoardDynamixel();
-
-/*    demo->readPosition( LEG_1, JOINT_0, kat0);
+    /* demo->readPosition( LEG_1, JOINT_0, kat0);
     demo->readPosition( LEG_1, JOINT_1, kat1);
     demo->readPosition( LEG_1, JOINT_2, kat2);
-
     demo->setSpeed( motorSpeed );
-
     demo->setComplianceSlope( motorComplianceSlope );
     demo->setOffset( motorOffset );
-
     demo->setPosition(LEG_0, polozeniePoczatkowe );
     //demo->setPosition(LEG_1, polozeniePoczatkowe );
     //demo->setPosition(LEG_2, polozeniePoczatkowe );
     //demo->setPosition(LEG_3, polozeniePoczatkowe );
     //demo->setPosition(LEG_4, polozeniePoczatkowe );
     //demo->setPosition(LEG_5, polozeniePoczatkowe );
-
     demo->readTorque(LEG_0, JOINT_0, moment0 );
-
     cout << "Katy: " << endl;
     cout << (kat0,*180)/M_PI << endl << (kat1*180)/M_PI << endl << (kat2*180)/M_PI << endl;
     cout << "Momenty: " << endl;
     cout << moment0 << endl << moment1 << endl << moment2 << endl;
-*/
-<<<<<<< HEAD
+    */
+    demo->setOffset(5,0,(-10*M_PI)/180);
     for(int i=0;i<6;i++)
     {
         demo->setPosition(i, 0, (0*M_PI)/180 );
         demo->setPosition(i, 1, (24*M_PI)/180 );
         demo->setPosition(i, 2, (-114*M_PI)/180 );
     }
-
-=======
-	demo->setOffset(5,0,(-10*M_PI)/180);
-    for(int i=0;i<6;i++)
-    {
-
-        demo->setPosition(i, 0, (0*M_PI)/180 );
-        demo->setPosition(i, 1, (24*M_PI)/180 );
-	demo->setPosition(i, 2, (-114*M_PI)/180 );
-    }
-
->>>>>>> 7a307717e72710a801769c7f78674263d0f3b432
     while (true){
         cout << "Noga = ";
         cin >> noga;
         cout << "Wezel = ";
         cin >> wezel;
-<<<<<<< HEAD
-        cout << "Kat = ";
-        cin >> kat;
-        demo->setPosition(noga, wezel, (kat*M_PI)/180 );
-=======
         cout << "Podatnosc = ";
         cin >> podatnosc;
         demo->setTorqueLimit(noga, wezel, podatnosc);
-	demo->readPosition(noga,wezel,readkat);
-    demo->readPositions(noga,readkatLeg);
-    cout<<"Odczytany kat: "<<readkat*180/M_PI<<endl;
-    cout<<"Odczytane katy w nodze: "<<readkatLeg[0]*180/M_PI<<" "<<readkatLeg[1]*180/M_PI<<" "<<readkatLeg[2]*180/M_PI<<endl;
->>>>>>> 7a307717e72710a801769c7f78674263d0f3b432
+        demo->readPosition(noga,wezel,readkat);
+        demo->readPositions(noga,readkatLeg);
+        cout<<"Odczytany kat: "<<readkat*180/M_PI<<endl;
+        cout<<"Odczytane katy w nodze: "<<readkatLeg[0]*180/M_PI<<" "<<readkatLeg[1]*180/M_PI<<" "<<readkatLeg[2]*180/M_PI<<endl;
     }
-
     return 0;
 }
