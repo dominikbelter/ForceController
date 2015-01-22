5ece5bb17eb12daa80d3c1c7070c91e517840cdb
Joregus <username>
2015-01-22 07:19:50 -0800

    moveplatform test

--- demoRobot.cpp
+++ demoRobot.cpp
@@ -6,6 +6,11 @@
 #include <stdio.h>
 #include "include/visualization/visualizerIrrlicht.h"
 #include <irrlicht.h>
+#include "include/board/board.h"
+#include "include/board/boardDynamixel.h"
+#include "3rdParty/dynamixel/dynamixel.h"
+#include "3rdParty/dynamixel/dxl_hal.h"
+
 /*
 Joregus
 Emil
@@ -36,6 +41,10 @@ int main( int argc, const char** argv )
         Visualizer* visualizer;
         visualizer = createVisualizerIrrlicht("configVisualization.xml", "TEST");
 
+        Board *demo = createBoardDynamixel();
+
+
+
 
 		std::vector<float_type> configuration, configuration2, Fz;
         // configuration2.push_back(18);
@@ -65,7 +74,34 @@ int main( int argc, const char** argv )
         configuration2 = Rob->movePlatformNeutral(testmoveplatform);
         //getchar();
 
-       visualizer->drawRobot(robotPose, configuration2);
+       //visualizer->drawRobot(robotPose, configuration2);
+
+
+       //leg 0
+            demo->setPosition(0, 0, configuration2[0] );
+            demo->setPosition(0, 1, configuration2[1] );
+            demo->setPosition(0, 2, configuration2[2] );
+       //leg 1
+            demo->setPosition(1, 0, configuration2[3] );
+            demo->setPosition(1, 1, configuration2[4] );
+            demo->setPosition(1, 2, configuration2[5] );
+       //leg 2
+            demo->setPosition(2, 0, configuration2[6] );
+            demo->setPosition(2, 1, configuration2[7] );
+            demo->setPosition(2, 2, configuration2[8] );
+       //leg 3
+            demo->setPosition(3, 0, configuration2[9] );
+            demo->setPosition(3, 1, configuration2[10] );
+            demo->setPosition(3, 2, configuration2[11] );
+        // leg 4
+            demo->setPosition(4, 0, configuration2[12] );
+            demo->setPosition(4, 1, configuration2[13] );
+            demo->setPosition(4, 2, configuration2[14] );
+        //leg 5
+            demo->setPosition(5, 0, configuration2[15] );
+            demo->setPosition(5, 1, configuration2[16] );
+            demo->setPosition(5, 2, configuration2[17] );
+
 
         return 0;
 
