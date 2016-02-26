c90ec23bfc8ce14fcb41b568a8966005c1f5116d
macSobiak <mac.sobiak@gmail.com>
2016-02-26 12:16:04 +0100

    contacts test2

--- smoothLegMotion.cpp
+++ smoothLegMotion.cpp
@@ -29,7 +29,7 @@ int main( int argc, const char** argv )
 
     while(true)
     {
-        if(readContact(5))
+        if(robot->readContact(5))
         {
             cout << "JEST" << endl;
         }
