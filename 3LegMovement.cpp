6a63b8acef9d6371a46aa03cab4ae13089368c7a
macSobiak <mac.sobiak@gmail.com>
2016-08-31 14:23:46 +0200

    test czytanie momety

--- 3LegMovement.cpp
+++ 3LegMovement.cpp
@@ -28,16 +28,20 @@ int main( int argc, const char** argv )
 
         board->setSpeed(motorSpeed);
 
-//        bool startMotion = false;
+        bool startMotion = false;
+        vector<float_type> readTorque(3);
+        while(!startMotion)
+        {
+            board->readTorque(5,readTorque);
+            for (int i=0; i<3; i++)
+            {
+                cout << "NOGA 5 WENZEL " << i << "   " << readTorque[i] << endl;;
+            }
 
-//        while(!startMotion)
-//        {
-//            for(int i=0; i<6; i++)
-//            {
-//                cout << "NOGA " << i << "  " << board->readContact(i) << endl;
-//            }
-//            usleep(1000000);
-//        }
+            cout << endl;
+
+            usleep(1000000);
+        }
 
         for(int i=0; i<6; i++)
         {
